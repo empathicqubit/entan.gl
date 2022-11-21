2e26e26962eab7da91d6422cde09351c08d83437
empathicqubit <empathicqubit@entan.gl>
2022-11-21 01:35:01 +0100

    explicit env set

--- scripts.js
+++ scripts.js
@@ -40,7 +40,7 @@ const scripts = {
             shell.config.fatal = true;
             const project_id = shell.exec('npm-run-all --silent tf:project_id').stdout.trim();
             console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-            shell.exec(`firebase deploy --only hosting --project ${project_id}`);
+            shell.exec(`firebase deploy --only hosting --project ${project_id}`, { env: { ...process.env } });
         },
     },
     tf: {
